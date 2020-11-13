2b0f68b29862faffe17255ba0cff3d8c0a4d7421
RafaelJaimeMoreno <rajaimor@gmail.com>
2020-11-13 18:04:40 +0100

    fixing anterior

--- resources/views/privilegios.blade.php
+++ resources/views/privilegios.blade.php
@@ -5,8 +5,8 @@
     <div class="text-center">
         <div class="error mx-auto" data-text="Permisos">Permisos</div>
         <p class="lead text-gray-800 mb-5">Página no disponible</p>
-        <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-        <a href="index.html">&larr; Back to Dashboard</a>
+        <p class="text-gray-500 mb-0">Necesitas más privilegios para acceder a este contenido</p>
+        <a href="index.html">Vuelve a la página principal</a>
     </div>
 </div>
 @stop
