63e99a8d90b20f82a35ab24422bea5a729556555
ignacio <ignacastrisa@gmail.com>
2020-11-17 21:25:07 +0100

    Update index.blade.php

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.principal')
 @section('title')
-<<<<<<< Updated upstream
     <title>Usurios</title>
-=======
-<title>Usuarios</title>
->>>>>>> Stashed changes
 @stop
 @section('content')
 <div class="container-fluid">
@@ -12,7 +8,6 @@
         <h1 class="h3 mb-0 text-gray-800">Usuarios</h1>
         <a href="{{ url('/')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Volver</a>
     </div>
-<<<<<<< Updated upstream
     <div class="table-responsive">
         <table class="table table-bordered">
             <thead>
@@ -26,6 +21,7 @@
                     <th>Tipo de usuario</th>
                     <th>Empresa</th>
                     <th>Ciclo</th>
+                    <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>
@@ -54,60 +50,6 @@
                 @endforeach
             </tbody>
         </table>
-=======
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Id del usuario</th>
-                <th>Nombre del usuario</th>
-                <th>Primer apellido</th>
-                <th>Telefono</th>
-                <th>email</th>
-                <th>Verificación email</th>
-                <th>Tipo de usuario</th>
-                <th>Empresa</th>
-                <th>Ciclo</th>
-                <th>Acciones</th>
-
-
-            </tr>
-        </thead>
-
-        <tbody>
-
-        @forelse( $usuarios as $usuario)
-            
-            <tr>
-
-                <td>{{ $usuario->id}} </td>
-                <td>{{ $usuario->name}}</td>
-                <td>{{ $usuario->firstname}} </td>
-                <td>{{ $usuario->phone}}</td>
-                <td>{{ $usuario->email}} </td>
-                <td>{{ $usuario->email_verified_at}}</td>
-                <td>{{ $usuario->type}} </td>
-                <td>{{ $usuario->find($usuario->id)->enterprises->name}}</td>
-                <td>{{ $usuario->find($usuario->id)->cycles->name}} </td>
-
-
-                <td>
-                   
-                    <a href="{{ url('/usuario/' .$usuario->id.'/edit') }}" class="btn btn-info btn-icon-split text">
-                        Editar
-                    </a>
-
-                    <form method="post" action="{{ url('/usuario/' .$usuario->id) }}">
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-                        <button type="submit" onclick="return confirm('¿Está seguro de querer borrar?');" class="btn btn-danger btn-icon-split">Borrar</button></form>
-                </td>
-            </tr>
-            @empty
-                    No hay usuarios disponibles
-            @endforelse
-        </tbody>
-    </table>
->>>>>>> Stashed changes
     {{$usuarios->links()}}
     </div>
     <a href="{{ url('/usuario/create') }}" class="btn btn-primary">
