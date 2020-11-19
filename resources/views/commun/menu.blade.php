f2c505ab0f675270009b93b0285ced3b23bdcd30
ignacio <ignacastrisa@gmail.com>
2020-11-19 11:54:40 +0100

    vamooooooooooo

--- resources/views/commun/menu.blade.php
+++ resources/views/commun/menu.blade.php
@@ -9,14 +9,14 @@
     @LoggedAD()
     <li>
         <a href="{{url('empresa')}}">
-            
+
             <i class="fa fa-building"></i>
             <span>Empresas</span>
         </a>
     </li>
     <li>
         <a href="{{url('ciclo')}}">
-            
+
             <i class="fa fa-circle"></i>
             <span>Ciclos</span>
         </a>
@@ -25,14 +25,14 @@
     @LoggedALyAD()
     <li>
         <a href="{{ url('/fichas') }}">
-            
+
             <i class="fa fa-file"></i>
             <span>Fichas de Seguimiento</span>
         </a>
     </li>
     <li>
         <a href="{{ url('/asistencia') }}">
-            
+
             <i class="fa fa-calendar"></i>
             <span>Asistencia</span>
         </a>
@@ -43,35 +43,29 @@
     @LoggedTEyAD()
     <li>
         <a href="{{ url('/tareas') }}">
-            
+
             <i class="fa fa-book"></i>
             <span>Tareas</span>
         </a>
     </li>
     <li>
         <a href="{{ url('/modulos') }}">
-            
+
             <i class="fa fa-rebel"></i>
             <span>Módulos</span>
         </a>
     </li>
     <li>
         <a href="{{ url('/resultados') }}">
-<<<<<<< Updated upstream
             <i class="fa fa-registered"></i>
             <span>Resultados de aprendizaje</span>
         </a>
     </li>
     <li>
         <a href="{{ url('/criterios') }}">
-            
+
             <i class="fa fa-copyright"></i>
             <span>Criterios</span>
-=======
-            
-            <i class="fa fa-calendar"></i>
-            <span>Resultados</span>
->>>>>>> Stashed changes
         </a>
     </li>
     @endLoggedTEyAD()
