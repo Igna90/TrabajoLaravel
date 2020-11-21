91bb5043c6664622ac8893449c7271c9a49d7eb0
RafaelJaimeMoreno <rajaimor@gmail.com>
2020-11-21 17:39:42 +0100

    Arreglado botón editar de modulos

--- resources/views/criterios/create.blade.php
+++ resources/views/criterios/create.blade.php
@@ -9,28 +9,31 @@
         <div class="box box-primary">
             <div class="box-header with-border">
                 <h2>{{ __("Crear criterios") }}</h2>
-                <hr />
             </div>
             @include('partials.errors')
             <div class="box-body">
                 <form action="{{ url('criterios')}}" method="post" enctype="multipart/form-data">
                     {{ csrf_field() }}
-    <label for="number">{{'Rra'}}</label>
-        <input type="text" name="ra_id" id="ra_id"/>
-        <br>
-        <label for="number">{{'Tarea'}}</label>
-        <input type="text" name="task_id" id="task_id"/>
-        <br>
-        <label for="number">{{'Word'}}</label>
-        <input type="text" name="word" id="word"/>
-        <br>
-        <label for="description">{{'Descripción'}}</label>
-        <textarea name="description" id="description" style="resize: none; display:inline-block; vertical-align:middle" rows="3" cols="40"></textarea>
-        <br>
-        <label for="number">{{'Mask'}}</label>
-        <input type="text" name="mark" id="mark"/>
-        <br>
-        <input type="submit" value="Agregar" class="btn btn-primary">
-    </form>
+                    <label for="number">{{'Rra'}}</label>
+                    <input type="text" name="ra_id" id="ra_id" />
+                    <br>
+                    <label for="number">{{'Tarea'}}</label>
+                    <input type="text" name="task_id" id="task_id" />
+                    <br>
+                    <label for="number">{{'Word'}}</label>
+                    <input type="text" name="word" id="word" />
+                    <br>
+                    <label for="description">{{'Descripción'}}</label>
+                    <textarea name="description" id="description" style="resize: none; display:inline-block; vertical-align:middle" rows="3" cols="40"></textarea>
+                    <br>
+                    <label for="number">{{'Mark'}}</label>
+                    <input type="text" name="mark" id="mark" />
+                    <br>
+                    <input type="submit" value="Editar" class="btn btn-info">
+                    <a href="{{url('empresa')}}" class="btn btn-primary"> Volver</a>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 @stop
