398faa5276abbd101da43c4ff424a7a3cd128cda
RafaelJaimeMoreno <rajaimor@gmail.com>
2020-11-21 20:24:27 +0100

    Añadido, cuando esté un te se muestren solo sus módulos

--- resources/views/modulos/index.blade.php
+++ resources/views/modulos/index.blade.php
@@ -26,7 +26,43 @@ Módulos
       </div>
       <div class="box-body table-responsive no-padding">
         <table id="tbl" class="table data-tables table-striped table-hover" cellspacing="0" width="100%">
-          @LoggedTEyAD()
+        @LoggedTE()
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th></th>
+              <th class="actions"></th>
+            </tr>
+          </tfoot>
+          <tbody>
+            @forelse($modulos as $modulo)
+            @if($modulo->cycle_id===auth()->user()->cycle_id)
+
+            <tr>
+              <td>{{$modulo-> name}}</td>
+              <td class="actions">
+                <ul class="list-inline" style="margin-bottom:0px;">
+                  <li><a href="{{ url('/modulos/'.$modulo->id.'/edit') }}" title="" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a></li>
+                  <li>
+                    <form method="post" action="{{url('/modulos/' .$modulo->id) }}">
+                      {{csrf_field()}}
+                      {{ method_field('DELETE')}}
+                      <button class="btn btn-danger btn-xs" type="submit" onclick="return confirm('¿Está seguro de querer borrar?');" class="btn btn-danger btn-icon-split"><i class="fa fa-trash"></i></button></form>
+                  </li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+          @endif
+          @empty
+          @endforelse
+          @endLoggedTE()
+          @LoggedAD()
           <thead>
             <tr>
               <th>Nombre</th>
@@ -59,10 +95,10 @@ Módulos
                 </ul>
               </td>
             </tr>
-            @empty
-            @endforelse
-            @endLoggedTEyAD()
           </tbody>
+          @empty
+          @endforelse
+          @endLoggedAD()
         </table>
         {{$modulos->links()}}
       </div>
