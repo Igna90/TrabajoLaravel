f0af9246496036ed53e383a735eb53df79c21fd3
ignacio <ignacastrisa@gmail.com>
2020-11-21 16:42:45 +0100

    Mejorada vista criterios

--- resources/views/empresa/edit.blade.php
+++ resources/views/empresa/edit.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.principal')
 @section('title')
-
 <title>Empresas</title>
 @stop
 @section('page-header')
@@ -10,24 +9,27 @@
     <div class="col-md-8">
         <div class="box box-primary">
             <div class="box-header with-border">
+                <h2>{{ __("Editar una empresa") }}</h2>
             </div>
-            <h2>{{ __("Editar una empresa") }}</h2>
-            <hr />
-            @include('partials.errors')
 
-            <form action="{{ url('/empresa/' .$empresa->id)}}" method="post" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                {{method_field('PATCH')}}
-                <label for="name">{{'Nombre de la empresa'}}</label>
-                <input type="text" name="name" id="name" value="{{$empresa-> name}}">
-                <br />
-                <br />
-                <label for="email">{{'Email de la empresa'}}</label>
-                <input type="email" name="email" id="email" value="{{$empresa-> email}}">
-                <br />
-                <br />
-                <input type="submit" value="Editar" class="btn btn-info">
-                <a href="{{url('empresa')}}" class="btn btn-primary"> Volver</a>
-            </form>
+            @include('partials.errors')
+            <div class="box-body">
+                <form action="{{ url('/empresa/' .$empresa->id)}}" method="post" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    {{method_field('PATCH')}}
+                    <label for="name">{{'Nombre de la empresa'}}</label>
+                    <input type="text" name="name" id="name" value="{{$empresa-> name}}">
+                    <br />
+                    <br />
+                    <label for="email">{{'Email de la empresa'}}</label>
+                    <input type="email" name="email" id="email" value="{{$empresa-> email}}">
+                    <br />
+                    <br />
+                    <input type="submit" value="Editar" class="btn btn-info">
+                    <a href="{{url('empresa')}}" class="btn btn-primary"> Volver</a>
+                </form>
+            </div>
         </div>
-        @stop
+    </div>
+</div>
+@stop
